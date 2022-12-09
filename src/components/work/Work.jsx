1378b1f3ce76d9cf24fe4801f7841efeb7a45a4a
jhan <jhancacossiop@gmail.com>
2022-12-08 22:13:50 -0500

    responsive 360px

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,6 +4,7 @@ import "./work.css";
 export default function Work() {
   return (
     <section className="section__work" id="section__work">
+      <p className="section__p--work">Work</p>
       <div className="section__div--container">
         <div className="section__div--card">Proyect </div>
         <div className="section__div--card"> Proyect </div>
